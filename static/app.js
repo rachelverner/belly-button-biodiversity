103705cd5c602d8fb21630f3d70b7b973df7f14e
Rachel Verner <rachelverner27@gmail.com>
2021-10-14 18:07:25 -0500

    Update app.js

--- static/app.js
+++ static/app.js
@@ -93,26 +93,6 @@ function buildPlot(){
       Plotly.newPlot('bubble', data, layout);  
 
 
-      // gauge plot 
-      var data = [
-        {
-          domain: { x: [0, 1], y: [0, 1] },
-          value: filterData2[0].wfreq,
-          title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
-          type: "indicator",
-          mode: "gauge",
-          gauge: { axis: { visible: true, range: [0, 9] } },
-        },
-      ];
-      
-      var layout = { 
-        width: 600, 
-        height: 500,
-        margin: { t: 0, b: 0 } };
-      
-      Plotly.newPlot('gauge', data, layout);
-
-
 
     });
 
